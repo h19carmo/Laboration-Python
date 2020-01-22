3b973fc29202e8c06571831ee8442a6c4c4af9a2
h19carmo <55438972+h19carmo@users.noreply.github.com>
2020-01-22 19:53:37 +0100

    u

--- Modules.py
+++ Modules.py
@@ -1,18 +1,19 @@
 def gissaTalet():
     import random
     nummer = random.randint(0, 100)
-    while True: 
-        print('Gissa ett tal mellan 0 och 100')
+    rundor = 1
+    while rundor < 20: 
+        print('Gissa ett tal mellan 0 och 100, du har 20 försök på dig att gissa rätt, annars dör du :)')
         gissning = int(input())
         if gissning == nummer:
             print('Du gissade rätt!, talet var', nummer)
+            print('Det tog dig', rundor, 'antal försök')
             break
         elif gissning < nummer:
-            print('Ditt tal är lägre än mitt tal, prova högre...')
+            print('Ditt tal är lägre än talet, prova högre...')
         elif gissning > nummer:
-            print ('Ditt tal är högre än mitt, prova lägre...')
+            print ('Ditt tal är högre än talet, prova lägre...')
+        rundor +=1
 
-
-def DelatMed():
-    
-        
+        if not rundor < 20:
+            print('Du har använt alla dina chanser, get pwned puny mortal')
